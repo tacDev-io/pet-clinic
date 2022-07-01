af53cb24cf0d63f0412fea08f0065b06ec85bf1a
cdickson <info@calebdickson.com>
2022-06-30 21:11:14 -0400

    Added Service interfaces for Owner, Pet and Vet

---
+++ pet-clinic-data/src/main/java/wws/petclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package wws.petclinic.services;
+
+import wws.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/wws/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package wws.petclinic.services;
+
+import wws.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
