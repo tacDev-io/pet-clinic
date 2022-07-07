e7ff5f59c7069cd2ba070459352999a854243a80
cdickson <info@calebdickson.com>
2022-07-07 19:35:32 -0400

    Added Owner Index Page and Controller

--- pet-clinic-web/src/main/java/wws/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/wws/petclinic/controllers/VetController.java
@@ -1,2 +1,13 @@
-package wws.petclinic.controllers;public class VetController {
+package wws.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
 }
