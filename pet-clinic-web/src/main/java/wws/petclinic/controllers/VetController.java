8abed72a87d80236502197e693b027f7f974641c
cdickson <info@calebdickson.com>
2022-07-07 23:19:23 -0400

    Updated map implementation to manage the setting of Id properties

--- pet-clinic-web/src/main/java/wws/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/wws/petclinic/controllers/VetController.java
@@ -1,14 +1,24 @@
 package wws.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import wws.petclinic.services.VetService;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        // RETRIEVES A LIST OF ALL VETS
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
